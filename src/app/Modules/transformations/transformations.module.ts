a808fc4160ec9b1cb28c87686eda6b37947d7b9b
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-11-19 12:13:19 -0600

    First Commit

---
+++ src/app/Modules/transformations/transformations.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TransformationsRoutingModule } from './transformations-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TransformationsRoutingModule
+  ]
+})
+export class TransformationsModule { }
---
+++ src/app/interfaces/character.ts
@@ -0,0 +1,11 @@
+export interface Character {
+    id: number
+    name: string
+    ki: number
+    maxKi: string
+    race: string
+    gender: string
+    description: string
+    image: string
+    affiliation: string
+}
