67c8951283bfadce8071ede96d6e2124c550b3c4
TRoyGms <roy89_gona@hotmail.com>
2024-11-19 13:51:18 -0600

    Agrega cambios relacionados con transformations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TransformationComponent } from '../app/Modules/transformations/transformations/transformations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/transformations', pathMatch: 'full' },
+  { path: 'transformations', component: TransformationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
